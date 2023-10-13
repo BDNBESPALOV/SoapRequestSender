a409705af366c7174e22ad67db410159bfc73008
Dmitry Bespalov <bdn.bespalov@gmail.com>
2023-10-13 17:23:56 +0300

    The

---
+++ README.md
@@ -0,0 +1,7 @@
+For windows execute:
+
+```"C:\Program Files\Java\jdk1.8.0_351\bin\java" -jar SoapRequestSender.jar```
+
+Recommended version:
+
+```java version "1.8.0_351"```
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
  import org.slf4j.Logger;
-
  import java.io.FileInputStream;
  import java.io.FileNotFoundException;
  import java.io.IOException;
@@ -21,17 +20,25 @@
             String BPM302P8004 = property.getProperty("BPM302P.8004");
             String BPM311P8004 = property.getProperty("BPM311P.8004");
             String BPM312P8004 = property.getProperty("BPM312P.8004");
-
+            String BPM303P8003 = property.getProperty("BPM303P.8003");
+            String BPM303P8004 = property.getProperty("BPM303P.8004");
+            String BPM313P8003 = property.getProperty("BPM313P.8003");
+            String BPM313P8004 = property.getProperty("BPM313P.8004");
+            String xmlResponse_log = property.getProperty("xmlResponse.log");
             String xmlRequest = property.getProperty("xmlRequest");
-            main.request(BPM301P8003,xmlRequest,"ASUGF-BPM301P.8003");
-            main.request(BPM301P8004,xmlRequest,"ASUGF-BPM301P.8004");
-            main.request(BPM302P8003,xmlRequest,"ASUGF-BPM302P.8003");
-            main.request(BPM302P8004,xmlRequest,"ASUGF-BPM302P.8004");
-            main.request(BPM311P8003,xmlRequest,"ASUGF-BPM311P.8003");
-            main.request(BPM311P8004,xmlRequest,"ASUGF-BPM311P.8004");
-            main.request(BPM312P8003,xmlRequest,"ASUGF-BPM312P.8003");
-            main.request(BPM312P8004,xmlRequest,"ASUGF-BPM312P.8004");
 
+            main.request(BPM301P8003,xmlRequest,"ASUGF-BPM301P.8003 time=", Boolean.valueOf(xmlResponse_log));
+            main.request(BPM301P8004,xmlRequest,"ASUGF-BPM301P.8004 time=", Boolean.valueOf(xmlResponse_log));
+            main.request(BPM302P8003,xmlRequest,"ASUGF-BPM302P.8003 time=", Boolean.valueOf(xmlResponse_log));
+            main.request(BPM302P8004,xmlRequest,"ASUGF-BPM302P.8004 time=", Boolean.valueOf(xmlResponse_log));
+            main.request(BPM311P8003,xmlRequest,"ASUGF-BPM311P.8003 time=", Boolean.valueOf(xmlResponse_log));
+            main.request(BPM311P8004,xmlRequest,"ASUGF-BPM311P.8004 time=", Boolean.valueOf(xmlResponse_log));
+            main.request(BPM312P8003,xmlRequest,"ASUGF-BPM312P.8003 time=", Boolean.valueOf(xmlResponse_log));
+            main.request(BPM312P8004,xmlRequest,"ASUGF-BPM312P.8004 time=", Boolean.valueOf(xmlResponse_log));
+            main.request(BPM303P8003,xmlRequest,"ASUGF-BPM303P.8003 time=", Boolean.valueOf(xmlResponse_log));
+            main.request(BPM303P8004,xmlRequest,"ASUGF-BPM303P.8004 time=", Boolean.valueOf(xmlResponse_log));
+            main.request(BPM313P8003,xmlRequest,"ASUGF-BPM313P.8003 time=", Boolean.valueOf(xmlResponse_log));
+            main.request(BPM313P8004,xmlRequest,"ASUGF-BPM313P.8004 time=", Boolean.valueOf(xmlResponse_log));
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
@@ -41,12 +48,20 @@
 
     }
 
-    private void request(String url,String xmlRequest,String hostname){
+    private void request(String url,String xmlRequest,String hostname, Boolean xmlResponse_log){
         SoapRequestSender sender = new  SoapRequestSender(url);
         long startTime = System.nanoTime();
         String xmlResponse = sender.sendSoapRequest(xmlRequest);
         long endTime = System.nanoTime();
-        //log.info(xmlResponse);
-        log.info(hostname+" "+((endTime - startTime) / 1000000) + " ms " );
+        if (xmlResponse_log)
+            log.info(xmlResponse);
+        String str = xmlResponse;
+        if(str.contains("2017-08-02T15:00:19+03:00")){
+            log.info("Successfully");
+            log.info("Response time for "+hostname+((endTime - startTime) / 1000000) + " ms " );
+        }
+        else {
+            log.info("Error");
+        }
     }
 }
